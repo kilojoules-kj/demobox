ddf5aed4aa96196951857a9ad85d0b123b99fbb2
tmaximun916 <69234520+tmaximun916@users.noreply.github.com>
2021-04-21 17:59:31 +0800

    Add files via upload
    added REST GET api

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs";
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Angular';
+  constructor(private http: HttpClient) {
+    
+  }
+
+  ngOnInit() {
+
+    let body = JSON.stringify({
+      "Result":{
+              "Ret":0,
+              "Total":2
+      },
+      "Projects":[{
+              "ID":3,
+              "Name":"Express",
+              "Description":"Express Project"
+      },
+      {
+              "ID":2,
+              "Name":"WaScada",
+              "Description":"WaScada Project"
+      }]
+    });
+
+    let obs = this.http.get('http://192.168.1.51/WaWebService/Json/ProjectList', {
+      headers: {'Content-Type': 'application/json', 'Authorization': 'YWRtaW46'}
+    });
+    obs.subscribe((response) => console.log(response))
+  }
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// HTTP request for REST api
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,6 +25,7 @@ import { MatButtonToggleModule } from '@angular/material/button-toggle';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonToggleModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
