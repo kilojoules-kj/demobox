11eb41241a301a67c57f34b7c3f033c8136c1600
Sha82 <82931176+Sha82@users.noreply.github.com>
2021-04-23 11:00:17 +0800

    Add files via upload

--- app/app.component.spec.ts
+++ app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
+
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -20,16 +21,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular'`, () => {
+  it(`should have as title 'myproject'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular');
+    expect(app.title).toEqual('myproject');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('myproject app is running!');
   });
 });
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,59 +1,14 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from "rxjs";
-import { ApiService } from './api.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
 export class AppComponent {
-  // constructor(private http: HttpClient, public _name?:string) {
-    
-  // }
-
-  constructor(private http: HttpClient) {
-
-  }
-
-  // get name() {
-  //   return this._name;
-  // }
-
-  // set name(value) {
-  //   this._name = value;
-  // }
- 
-  test = () => {
-    console.log("test")
-  }
-
-  getTagValue = (tagname:string) => {
-    let body = JSON.stringify({
-      "Tags":[{
-        "Name":tagname
-      }]
-    })
-
-    let obs = this.http.get('http://192.168.1.51/WaWebService/Json/ProjectList', {
-      headers: {'Content-Type': 'application/json', 'Authorization': 'YWRtaW46'}
-    });
-    obs.subscribe((response) => console.log(response))
-  }
-
-  setTagValue = (tagname:string, value:number) => {
-    let body = JSON.stringify({
-      "Tags":[{
-        "Name":tagname,
-        "Value":value
-      }]
-    });
-    console.log(body)
-    let obs = this.http.post('http://192.168.1.51/WaWebService/Json/SetTagValue/FirstProj', body, {
-      headers: {'Content-Type': 'application/json', 'Authorization': 'YWRtaW46'}
-    });
-    obs.subscribe((response) => console.log(response))
-  }
+  title = 'myproject';
 }
+
+
+
+
