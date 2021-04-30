4ed3fc430338cc27ec40a09b5ea758e6b092305c
tmaximun916 <tmaximun916@gmail.com>
2021-04-30 16:04:12 +0800

    added logo and qr code to navbar

--- app/app.component.ts
+++ app/app.component.ts
@@ -17,9 +17,13 @@ export class AppComponent {
   x:any;
   temp_value:any;
   override_dist_value!: number;
-  uptime_percentage!:number;
-  downtime_percentage!: number;
-  errortime_percentage!:number;
+  test:number | undefined;
+  Buzzer:any;
+
+  uptime_total:any;
+  uptime_green:any;
+  downtime_amber:any;
+  downtime_red:any;
 
   ngOnInit() {
     this.getTempValue;
@@ -30,17 +34,36 @@ export class AppComponent {
     this.calculateErrortime;
   }
 
+  Overlay = false;
+  state = false;
+
+  overlay_off() {
+    this.Overlay = false;
+  }
+
+  overlay_on() {
+    this.Overlay = true;
+  }
+
   //this pops up a page to stop further input if a error is detected
   errorCheck = setInterval(() => {
-    let Buzzer:any = this.getTagValue("Buzzer"); 
-    if (Buzzer == 1) {
+    this.getTagValue("Buzzer"); 
+    
+    if (this.Buzzer == 1) {
       // error overlay
-      console.log("ERROR TRIGGER, STOP OPERATION!")
-    }; 
-  }, 1000)
+      console.log("ERROR TRIGGER, STOP OPERATION!");
+      if (this.state == false) {
+        this.overlay_on()
+        this.state = true;
+      }
+    } else {
+      this.state = false;
+      this.overlay_off()
+    } 
+  }, 300)
 
   // this is to simulate a dist sensor error
-  simulateDistError = setInterval(() => {this.setDistValue(this.override_dist_value)}, 1000)
+  simulateDistError = setInterval(() => {this.setDistValue(this.override_dist_value)}, 500)
   setDistValue = (value: number) => {
     if (value > 600) {
       this.setTagValue("error_alert", 1);
@@ -48,26 +71,24 @@ export class AppComponent {
   }
 
   // this is for getting temp value
-  getTempValue = setInterval(() => {this.temp_value = this.getTagValue("temperature")}, 1000);
+  getTempValue = setInterval(() => {this.temp_value = this.getTagValue("temperature")}, 3000);
 
   // get and calculate the percentage of time that green led is up
   calculateUptime = setInterval(() => {
-    let uptime_green = this.getTagValue("uptime_green");
-    let uptime_total = this.getTagValue("uptime_total");
-    this.uptime_percentage = (uptime_green / uptime_total) *100;
-  }, 1000);
+    this.uptime_green = this.getTagValue("uptime_green");
+    this.uptime_total = this.getTagValue("uptime_total");
+  }, 7000);
 
   calculateDowntime = setInterval(() => {
-    let uptime_amber = this.getTagValue("uptime_green");
-    let uptime_total = this.getTagValue("uptime_total");
-    this.downtime_percentage = (uptime_amber / uptime_total) *100;
-  }, 1000);
+    this.downtime_amber = this.getTagValue("downtime_amber");
+    this.uptime_total = this.getTagValue("uptime_total");
+    //this.test = Number(this.downtime_amber) //(100*parseFloat(this.downtime_amber)/parseFloat(this.uptime_total))
+  }, 7000);
 
   calculateErrortime = setInterval(() => {
-    let uptime_red = this.getTagValue("uptime_green");
-    let uptime_total = this.getTagValue("uptime_total");
-    this.errortime_percentage = (uptime_red / uptime_total) *100;
-  }, 1000);
+    this.downtime_red = this.getTagValue("downtime_red");
+    this.uptime_total = this.getTagValue("uptime_total");
+  }, 7000);
 
   lightcontrol(tagname: string) {
     this.setTagValue("towerlight_green", 0);
@@ -100,8 +121,10 @@ export class AppComponent {
     this.motorOff = !this.motorOff;
   }
   
+  nav_button = false;
+
   nav_button_toggle() {
-    // do nothing yet
+    this.nav_button = !this.nav_button;
   }
 
   getTagValue = (tagname:string) => {
@@ -118,10 +141,27 @@ export class AppComponent {
     let obs = this.http.post('http://192.168.1.51/WaWebService/Json/GetTagValue/FirstProj', body, {
       headers: {'Content-Type': 'application/json', 'Authorization': 'YWRtaW46'}
     });
-    obs.subscribe((response) => { 
-      this.x = (JsonValues(response));
+    obs.subscribe((response) => {
+      switch (tagname) {
+        case "temperature":
+          this.temp_value = (JsonValues(response));
+          break;
+        case "uptime_green":
+          this.uptime_green = (JsonValues(response));        
+          break;
+        case "downtime_amber":
+          this.downtime_amber = (JsonValues(response));
+          break;
+        case "downtime_red":
+          this.downtime_red = (JsonValues(response));
+          break;
+        case "uptime_total":
+          this.uptime_total = (JsonValues(response));
+          break;
+        case "Buzzer":
+          this.Buzzer = (JsonValues(response));
+        }     
     });
-    return this.x;
   }
 
   setTagValue = (tagname:string, value:number) => {
@@ -131,7 +171,6 @@ export class AppComponent {
         "Value":value
       }]
     });
-    console.log(body)
     let obs = this.http.post('http://192.168.1.51/WaWebService/Json/SetTagValue/FirstProj', body, {
       headers: {'Content-Type': 'application/json', 'Authorization': 'YWRtaW46'}
     });
