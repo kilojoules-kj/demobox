d0a3a9d3ddc9d7b250cfb3430b83051891999101
tmaximun916 <tmaximun916@gmail.com>
2021-05-18 15:01:23 +0800

    simplify stuff

--- README.md
+++
@@ -1,3 +0,0 @@
-# demobox
-
-this repo is strictly for Angular stuff only! Do not create a pull request TO or FROM main branch
--- app.component.ts
+++ app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, Subject } from "rxjs";
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -14,14 +15,22 @@ export class AppComponent {
   }
   title = "Angular";
 
-  x:any;
   temp_value:any;
+  motor_value: any;
   override_dist_value!: number;
-  uptime_percentage!:number;
-  downtime_percentage!: number;
-  errortime_percentage!:number;
+  Buzzer:any;
+
+  uptime_total:any;
+  uptime_green:any;
+  downtime_amber:any;
+  downtime_red:any;
+
+  percentage_uptime = 0;
+  percentage_downtime = 0;
+  percentage_errortime = 0;
 
   ngOnInit() {
+    this.getMotorValue;
     this.getTempValue;
     this.simulateDistError;
     this.errorCheck;
@@ -30,44 +39,68 @@ export class AppComponent {
     this.calculateErrortime;
   }
 
-  //this pops up a page to stop further input if a error is detected
-  errorCheck = setInterval(() => {
-    let Buzzer:any = this.getTagValue("Buzzer"); 
-    if (Buzzer == 1) {
-      // error overlay
-      console.log("ERROR TRIGGER, STOP OPERATION!")
-    }; 
-  }, 1000)
+  Overlay = false;
+  state = false;
 
-  // this is to simulate a dist sensor error
-  simulateDistError = setInterval(() => {this.setDistValue(this.override_dist_value)}, 1000)
-  setDistValue = (value: number) => {
-    if (value > 600) {
-      this.setTagValue("error_alert", 1);
-    }
+  overlay_off() {
+    this.Overlay = false;
   }
 
-  // this is for getting temp value
-  getTempValue = setInterval(() => {this.temp_value = this.getTagValue("temperature")}, 1000);
+  overlay_on() {
+    this.Overlay = true;
+  }
 
+  //this pops up a page to stop further input if a error is detected
+  errorCheck = setInterval(() => {
+    this.getTagValue("Buzzer"); 
+    
+    if (this.Buzzer == 1) {
+      // error overlay
+      console.log("ERROR TRIGGER, STOP OPERATION!");
+      if (this.state == false) {
+        this.overlay_on()
+        this.state = true;
+      }
+    } else {
+      this.state = false;
+      this.overlay_off()
+    } 
+  }, 300)
+
+  // this is to simulate a dist sensor error
+  simulateDistError = setInterval(() => {
+    if (this.override_dist_value > 600) {
+      this.setTagValue("error_alert", 1);
+    }
+  }, 500)
+
+  // this is for getting temp value
+  getTempValue = setInterval(() => {this.getTagValue("temperature")}, 5000);
+  
   // get and calculate the percentage of time that green led is up
   calculateUptime = setInterval(() => {
-    let uptime_green = this.getTagValue("uptime_green");
-    let uptime_total = this.getTagValue("uptime_total");
-    this.uptime_percentage = (uptime_green / uptime_total) *100;
-  }, 1000);
+    this.getTagValue("uptime_green");
+    this.getTagValue("uptime_total");
+
+    let value:number = +this.uptime_green;
+    this.percentage_uptime = 100*value/this.uptime_total;
+  }, 2500);
 
   calculateDowntime = setInterval(() => {
-    let uptime_amber = this.getTagValue("uptime_green");
-    let uptime_total = this.getTagValue("uptime_total");
-    this.downtime_percentage = (uptime_amber / uptime_total) *100;
-  }, 1000);
+    this.getTagValue("downtime_amber");
+    this.getTagValue("uptime_total");
+
+    let value:number = +this.downtime_amber;
+    this.percentage_downtime = 100*value/this.uptime_total;
+  }, 2500);
 
   calculateErrortime = setInterval(() => {
-    let uptime_red = this.getTagValue("uptime_green");
-    let uptime_total = this.getTagValue("uptime_total");
-    this.errortime_percentage = (uptime_red / uptime_total) *100;
-  }, 1000);
+    this.getTagValue("downtime_red");
+    this.getTagValue("uptime_total");
+
+    let value:number = +this.downtime_red;
+    this.percentage_errortime = 100*value/this.uptime_total;
+  }, 2500);
 
   lightcontrol(tagname: string) {
     this.setTagValue("towerlight_green", 0);
@@ -75,6 +108,18 @@ export class AppComponent {
     this.setTagValue("towerlight_red", 0);
     this.setTagValue(tagname, 1);
   }
+
+  getMotorValue = setInterval(() => {
+    this.getTagValue("Motor")
+
+    if (this.motor_value == 1) {
+      this.motorOn = true;
+      this.motorOff = false;
+    } else {
+      this.motorOff = true;
+      this.motorOn = false;
+    }
+  }, 300);
 
   motorOff = true;
   motorOn = false;
@@ -100,8 +145,10 @@ export class AppComponent {
     this.motorOff = !this.motorOff;
   }
   
+  nav_button = false;
+
   nav_button_toggle() {
-    // do nothing yet
+    this.nav_button = !this.nav_button;
   }
 
   getTagValue = (tagname:string) => {
@@ -118,10 +165,30 @@ export class AppComponent {
     let obs = this.http.post('http://192.168.1.51/WaWebService/Json/GetTagValue/FirstProj', body, {
       headers: {'Content-Type': 'application/json', 'Authorization': 'YWRtaW46'}
     });
-    obs.subscribe((response) => { 
-      this.x = (JsonValues(response));
+    obs.subscribe((response) => {
+      switch (tagname) {
+        case "temperature":
+          this.temp_value = (JsonValues(response));
+          break;
+        case "uptime_green":
+          this.uptime_green = (JsonValues(response));        
+          break;
+        case "downtime_amber":
+          this.downtime_amber = (JsonValues(response));
+          break;
+        case "downtime_red":
+          this.downtime_red = (JsonValues(response));
+          break;
+        case "uptime_total":
+          this.uptime_total = (JsonValues(response));
+          break;
+        case "Buzzer":
+          this.Buzzer = (JsonValues(response));
+          break;
+        case "motor":
+        this.motor_value = (JsonValues(response));
+        }
     });
-    return this.x;
   }
 
   setTagValue = (tagname:string, value:number) => {
@@ -131,7 +198,6 @@ export class AppComponent {
         "Value":value
       }]
     });
-    console.log(body)
     let obs = this.http.post('http://192.168.1.51/WaWebService/Json/SetTagValue/FirstProj', body, {
       headers: {'Content-Type': 'application/json', 'Authorization': 'YWRtaW46'}
     });
