801f2b8eb96c4153600da80d83ae08d5b250ab86
tmaximun916 <tmaximun916@gmail.com>
2021-06-08 11:05:38 +0800

    another crazy overhaul to graph_functions, Buttons is now the time calculating class, when object is created, time is stored

--- graph_functions.py
+++ graph_functions.py
@@ -6,26 +6,41 @@ import math
 myobj = rest.myclass()
 
 class Buttons:
+    # object is created
     def __init__(self, tag_send, tag_datetime = None):
-        self.tag_send = tag_send
-        self.tag_datetime = tag_datetime
-        # mark the start time down first
-        self.time_start = time.time()
+        self.tag_send = tag_send # this tag is for normal time in seconds
+        self.tag_datetime = tag_datetime # this tag is for time converted into datetime format
+        self.time_start = time.time() # mark the start time down first
 
+    # object is deleted
     def __del__(self):
-        # mark the end time
-        self.time_end = time.time()
-        self.time_duration = self.time_end - self.time_start
-        # print out the duration when the object is destroyed
-        print(self.time_duration)
-        data = myobj.receive_restful("downtime_red")
-        myobj.write_restful(self.tag_send, (self.time_duration)+data)
+        self.time_duration = time.time() - self.time_start # calculate the time that object has been alive
+        time_in_seconds = self.time_duration + myobj.receive_restful(self.tag_send) # get normal time
 
-        self.const_time = myobj.receive_restful(self.tag_send)
-        self.SEC = math.floor(self.time_duration + self.const_time)
+        # convert normal seconds time into bigger units of measurement
+        self.SEC = math.floor(time_in_seconds)
         self.MIN = math.floor(self.SEC/60)
         self.HR = math.floor(self.MIN/60)
-        myobj.write_restful_text(self.tag_datetime, myclass.datetime(self, self.SEC%60, self.MIN%60, self.HR))
+
+        myobj.write_restful(self.tag_send, time_in_seconds) # update the normal time
+        myobj.write_restful_text(self.tag_datetime, Buttons.datetime(self, self.SEC%60, self.MIN%60, self.HR)) # update datetime
+
+    def datetime(self, seconds, minutes, hours):
+        datetime = []
+        if len(str(hours)) < 2:
+            datetime.append("0")
+        datetime.append(hours)
+        datetime.append(":")
+        if len(str(minutes)) < 2:
+            datetime.append("0")
+        datetime.append(minutes)
+        datetime.append(":")
+        if len(str(seconds)) < 2:
+            datetime.append("0")
+        datetime.append(seconds)
+        datetimeStr = "".join(map(str, datetime))
+        
+        return datetimeStr
 
 
 class storeTime():
@@ -44,17 +59,10 @@ class myclass():
     const_time = myobj.receive_restful("uptime_total")
 
     def __init__(self, tag_receive, tag_send, tag_datetime = None):
-        self.tag_receive = tag_receive
+        self.tag_receive = tag_receive  # this tag is used for the light sensor tag
         self.tag_send = tag_send
         self.tag_datetime = tag_datetime
-        self.counter = False
-        self.sensor_start = None
-        self.sensor_end = None
-        self.sensor_uptime = None
         self.const_time = None
-        self.SEC = None
-        self.MIN = None
-        self.HR = None
 
     def uptime(self):
         try:
@@ -63,8 +71,7 @@ class myclass():
             print("generic error, please check")
             return
         if data > 200:
-            button = Buttons("uptime_green", "datetime_green")
-
+            button = Buttons(self.tag_send, self.tag_datetime)
 
     def total(self):
         t_end = None
@@ -72,22 +79,7 @@ class myclass():
         SEC = math.floor((t_end - myclass.t_start) + myclass.const_time)
         MIN = math.floor(SEC/60)
         HR = math.floor(MIN/60)
-        myobj.write_restful_text("datetime_total", myclass.datetime(self, SEC%60, MIN%60, HR))
+        myobj.write_restful_text("datetime_total", Buttons.datetime(self, SEC%60, MIN%60, HR))
         return((t_end-myclass.t_start)+myclass.const_time)
 
-    def datetime(self, seconds, minutes, hours):
-        datetime = []
-        if len(str(hours)) < 2:
-            datetime.append("0")
-        datetime.append(hours)
-        datetime.append(":")
-        if len(str(minutes)) < 2:
-            datetime.append("0")
-        datetime.append(minutes)
-        datetime.append(":")
-        if len(str(seconds)) < 2:
-            datetime.append("0")
-        datetime.append(seconds)
-        datetimeStr = "".join(map(str, datetime))
-        
-        return datetimeStr
+    
